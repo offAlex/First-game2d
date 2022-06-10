8fb9ee594c18223578f4cf8e350f588dd6a495c9
Alexandr <off.sokal.al@gmail.com>
2022-06-10 23:06:38 +0300

    add platrofm transform

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    [SerializeField] private float speed = 0.1f;
+    [SerializeField] private bool moveing;
+
+    void FixedUpdate()
+    {
+        if (transform.position.y>3.5f){
+            moveing = false;
+        }
+        if (transform.position.y<-4.5){
+            moveing = true;
+        }
+
+        if(moveing){
+            transform.position = new Vector2(transform.position.x,transform.position.y+speed);
+        }
+        else{
+            transform.position = new Vector2(transform.position.x,transform.position.y-speed);
+        }
+    }
+
+}
