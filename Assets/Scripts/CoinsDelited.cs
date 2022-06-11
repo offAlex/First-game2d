aec3ef24520c43db6a039c1534b0e7bb3c75a739
Alexandr <off.sokal.al@gmail.com>
2022-06-11 06:20:37 +0300

    add particle systems

--- Assets/Scripts/CoinsDelited.cs
+++ Assets/Scripts/CoinsDelited.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CoinsDelited : MonoBehaviour
 {   
+
+    
     // Start is called before the first frame update
     void Start()
-    {
+    {   
         StartCoroutine(DeliteCoins());
     }
 
         IEnumerator DeliteCoins()
     {   
         yield return new WaitForSeconds(7);
+
         Destroy(this.gameObject);
     }
 }
